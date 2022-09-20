1ae069c8ef24b4e532fd8320bd6870943a7dc240
sumin <ysm2706@naver.com>
2022-09-20 22:26:45 +0900

    chore : 회원가입 할 때 회원정보 추가,비밀번호 재확인 및 이메일로 회원가입하기, 로그인 화면 tailwind 적용

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, session
 from flask import request
 from flask import redirect
+import re
 
 from db import db
 market_db = db
@@ -18,11 +19,8 @@ def main():
 
 @app.route("/home")
 def home():
-    if "id" in session:
-        items = market_db.items.find()
-        return render_template("home.html")
-    else:
-        return redirect(url_for('signin'))
+    db_items = market_db.items.find()
+    return render_template("home.html",items=db_items)
 
 
 @app.route('/signin', methods=['POST', 'GET'])
@@ -54,15 +52,23 @@ def signup():
     else:
         signup_id = request.form['signup_id']
         signup_pw = request.form['signup_pw']
+        signup_check_pw = request.form['signup_check_pw']
+        signup_name = request.form['signup_name']
+        signup_nickname = request.form['signup_nickname']
 
-        if signup_id == "" or signup_pw == "":
+        p = re.compile("^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+
+        if signup_id == "" or signup_pw == "" or signup_name == "" or signup_nickname == "":
             return render_template("signup.html", signup_success = False)
+        elif not p.findall(signup_id):
+            return render_template("signup.html", signup_check_id = False)
         elif market_db.users.find_one({'id' : signup_id}):
             return render_template("signup.html", signup_same = True)
+        elif signup_pw != signup_check_pw:
+            return render_template("signup.html", signup_same_pw = True)
         else:
-            market_db.users.insert_one({'id' : signup_id, 'pwd' : signup_pw})
+            market_db.users.insert_one({'id' : signup_id, 'pwd' : signup_pw, 'name' : signup_name, 'nickname' : signup_nickname})
             return render_template("signup.html", signup_success = True)
-
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, debug=True)
