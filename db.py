9440a52d7268a3030fb730795361bb171f0342c7
unknown <junman95@naver.com>
2022-09-20 20:34:08 +0900

    fix : 병합충돌

--- db.py
+++ db.py
@@ -1,8 +1,4 @@
 from pymongo import MongoClient
 
 client = MongoClient("localhost", 27017)
-<<<<<<< HEAD
 db = client.dbjunglemarket
-=======
-db = client.dbjunglemarket
->>>>>>> 0bf15644e24123ea2be2a9f830aeb144b012b558
